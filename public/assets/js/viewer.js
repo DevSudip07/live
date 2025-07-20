396e52bb9ab2a3dbf310756bc5e299d6270f15b5
DevSudip07 <sudipdasghosh6@gmail.com>
2025-07-20 20:37:02 +0530

    ui correction

--- public/assets/js/viewer.js
+++ public/assets/js/viewer.js
@@ -6,23 +6,26 @@ const ansvideo = document.querySelector(".ans");
 const quesvideo = document.querySelector(".ques");
 const trybutton = document.querySelector(".retry");
 
+quesvideo.setAttribute('autoplay', 'true');
+
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     if (userInput.value == "") {
         button.textContent = "BSDK Pehle Answer Likh 🤬";
+        quesvideo.play();
     } else {
         button.textContent = "Cheking...";
     }
     setTimeout(() => {
         if (userInput.value != "6") {
             quesvideo.style.display = "none";
-            ansvideo.src = "./assets/video/wrong.mp4";
+            ansvideo.src = "./assets/video/wrong.webm";
             ansvideo.setAttribute('autoplay', 'true');
             quesvideo.pause();
             button.textContent = "Laure ho tum, woh bhi tede wale";
         } else {
             quesvideo.style.display = "none";
-            ansvideo.src = "./assets/video/correct.mp4";
+            ansvideo.src = "./assets/video/correct.webm";
             ansvideo.setAttribute('autoplay', 'true');
             quesvideo.pause();
             button.textContent = "Sahi pakde hai";
